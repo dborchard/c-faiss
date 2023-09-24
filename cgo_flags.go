94eeb573e0544a89fc3d74747ffa798c52d44d1a
Arjun Sunil Kumar <arjunsk@users.noreply.github.com>
2023-09-24 01:51:23 -0700

    Add faiss in Makefile

--- cgo_flags.go
+++ cgo_flags.go
@@ -1,6 +1,7 @@
 package faiss
 
-// #cgo CPPFLAGS: -Iinternal/c_api -Iinternal/faiss
+//#cgo CPPFLAGS: -Iinternal/c_api -Iinternal/faiss
+//#cgo CFLAGS: -Iinternal/c_api -Iinternal/faiss
 // #cgo darwin LDFLAGS: -Wl,-undefined -Wl,dynamic_lookup
 // #cgo !darwin LDFLAGS: -Wl,-unresolved-symbols=ignore-all
 import "C"
